5100790ee58b64050b215583e89c00b1baedc4e4
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-03-14 15:29:09 +0700

    Fix
    resolve some problems on packages

--- BackEnd/foodmarket-backend-master/public/js/app.js
+++ BackEnd/foodmarket-backend-master/public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\foodmarket-backend\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\foodmarket-backend\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/FoodMarketApp/BackEnd/foodmarket-backend-master/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/FoodMarketApp/BackEnd/foodmarket-backend-master/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
